609ea2688085de76e7a9d74f97d08a4cc3741c58
Smdxyz <121522562+Smdxyz@users.noreply.github.com>
2025-01-07 17:17:30 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('./database');
+const apiRoutes = require('./routes/apiRoutes');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml');
+const swaggerDocument = YAML.load('./docs/swagger.yaml');
 
 const app = express();
 const port = 3000;
@@ -11,64 +12,14 @@ const port = 3000;
 // Setup body-parser
 app.use(bodyParser.json());
 
+// Serve static files (frontend)
+app.use(express.static('public')); // Untuk melayani file statis seperti index.html
+
 // Swagger docs
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// Add new API
-app.post('/api/add', (req, res) => {
-  const { name, url, limit } = req.body;
-  db.run(
-    'INSERT INTO apis (name, url, limit) VALUES (?, ?, ?)',
-    [name, url, limit || 1000],
-    function(err) {
-      if (err) {
-        return res.status(500).send({ message: 'Failed to add API' });
-      }
-      res.status(201).send({ id: this.lastID, name, url, limit });
-    }
-  );
-});
-
-// Delete API
-app.delete('/api/delete/:id', (req, res) => {
-  const { id } = req.params;
-  db.run('DELETE FROM apis WHERE id = ?', [id], function(err) {
-    if (err) {
-      return res.status(500).send({ message: 'Failed to delete API' });
-    }
-    res.status(200).send({ message: 'API deleted' });
-  });
-});
-
-// Update API limit
-app.put('/api/limit/:id', (req, res) => {
-  const { id } = req.params;
-  const { limit } = req.body;
-  db.run(
-    'UPDATE apis SET limit = ? WHERE id = ?',
-    [limit, id],
-    function(err) {
-      if (err) {
-        return res.status(500).send({ message: 'Failed to update limit' });
-      }
-      res.status(200).send({ message: 'API limit updated' });
-    }
-  );
-});
-
-// Get error and success stats
-app.get('/api/stats/:id', (req, res) => {
-  const { id } = req.params;
-  db.get('SELECT * FROM apis WHERE id = ?', [id], (err, row) => {
-    if (err || !row) {
-      return res.status(404).send({ message: 'API not found' });
-    }
-    res.status(200).send({
-      errorStats: row.errorStats,
-      successStats: row.successStats,
-    });
-  });
-});
+// API routes
+app.use('/api', apiRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
